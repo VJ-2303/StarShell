4b684cfeb8543d002189898363b8ea7b40067e3c
VJ-2303 <vanaraj1018@gmail.com>
2026-03-08 22:12:40 +0530

    New

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,21 @@
+use std::io::{self, Write};
 
+fn main() {
+    loop {
+        print!("minishell > ");
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+
+        let command = input.trim();
+
+        if command.is_empty() {
+            continue;
+        }
+
+        let args: Vec<&str> = command.split_whitespace().collect();
+
+        println!("Parsed arguments: {:?}", args);
+    }
+}
