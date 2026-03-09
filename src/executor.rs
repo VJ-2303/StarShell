99410eb31404d1d3328d3fc46f381867ee76957d
VJ-2303 <vanaraj1018@gmail.com>
2026-03-09 19:30:35 +0530

    Added output redirection using > character, the standard output of the command will be rewired to the file provided by the user

--- src/executor.rs
+++ src/executor.rs
@@ -1,6 +1,7 @@
 use std::env;
+use std::fs::File;
 use std::io;
-use std::process::Command;
+use std::process::{Command, Stdio};
 
 pub fn execute_command(args: &[&str]) -> Result<(), io::Error> {
     match args[0] {
@@ -15,8 +16,25 @@ pub fn execute_command(args: &[&str]) -> Result<(), io::Error> {
             std::process::exit(0);
         }
         _ => {
-            let mut child = Command::new(args[0]).args(&args[1..]).spawn()?;
-            child.wait()?;
+            if let Some(pos) = args.iter().position(|&s| s == ">") {
+                if pos + 1 >= args.len() {
+                    eprintln!("syntax error: unexpected token at the end");
+                    return Ok(());
+                }
+                let filename = args[pos + 1];
+                let command_args = &args[..pos];
+
+                let file = File::create(filename)?;
+
+                let mut child = Command::new(args[0])
+                    .args(&command_args[1..])
+                    .stdout(Stdio::from(file))
+                    .spawn()?;
+                child.wait()?;
+            } else {
+                let mut child = Command::new(args[0]).args(&args[1..]).spawn()?;
+                child.wait()?;
+            }
         }
     }
     Ok(())
