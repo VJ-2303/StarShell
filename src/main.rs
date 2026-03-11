1490cdeecad450bd19a98b2112d3bee37f1ce82c
VJ-2303 <vanaraj1018@gmail.com>
2026-03-11 15:58:14 +0530

    Enabled raw mode and handled user commands using an buffer

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,34 @@
 use std::{
     env,
-    io::{self, Write},
+    io::{self, Read, Write},
 };
 mod error;
 mod executor;
 mod parser;
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 use executor::execute_command;
+
+fn read_line() -> String {
+    enable_raw_mode().unwrap();
+    let mut buffer = String::new();
+
+    for byte in io::stdin().bytes() {
+        let b = byte.unwrap();
+
+        if b == 13 {
+            print!("\r\n");
+            break;
+        } else {
+            let c = b as char;
+            buffer.push(c);
+            print!("{}", c);
+            io::stdout().flush().unwrap();
+        }
+    }
+    disable_raw_mode().unwrap();
+
+    buffer
+}
 
 fn main() {
     if let Err(e) = ctrlc::set_handler(|| {
@@ -25,8 +48,7 @@ fn main() {
         print!("{} ❯ ", prompt_path);
         io::stdout().flush().unwrap();
 
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).unwrap();
+        let input = read_line();
 
         let args: Vec<&str> = input.trim().split_whitespace().collect();
 
